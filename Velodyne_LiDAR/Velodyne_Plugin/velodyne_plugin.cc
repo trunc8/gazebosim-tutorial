1f34d8c78919783a4ef794dd47590e5c22049dca
trunc8_Nautilu5 <sahasiddharth611@gmail.com>
2020-07-20 03:07:49 +0530

    Added ability to pass velocity via sdf and via message passing(using API)

--- Velodyne_LiDAR/Velodyne_Plugin/velodyne_plugin.cc
+++ Velodyne_LiDAR/Velodyne_Plugin/velodyne_plugin.cc
@@ -3,6 +3,8 @@
 
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
 
 namespace gazebo{
 	class VelodynePlugin : public ModelPlugin
@@ -38,19 +40,62 @@ namespace gazebo{
 		  this->model->GetJointController()->SetVelocityPID(
 		      this->joint->GetScopedName(), this->pid);
 
-		  // Set the joint's target velocity. This target velocity is just
-		  // for demonstration purposes.
+		  // Default to zero velocity.
+		  double velocity = 0;
+
+		  // Check that the velocity element exists, then read the value.
+		  if (_sdf->HasElement("velocity"))
+		  	velocity = _sdf->Get<double>("velocity");
+
+		  // Set the joint's target velocity.
 		  this->model->GetJointController()->SetVelocityTarget(
-		      this->joint->GetScopedName(), 10.0);
+		      this->joint->GetScopedName(), velocity);
+		  
+
+		  // Create the node
+			this->node = transport::NodePtr(new transport::Node());
+			this->node->Init(this->model->GetWorld()->Name());
+
+			// Create a topic name
+			std::string topicName = "~/" + this->model->GetName() + "/vel_cmd";
+
+			// Subscribe to the topic, and register a callback
+			this->sub = this->node->Subscribe(topicName,
+			   &VelodynePlugin::OnMsg, this);
 		}
+
+		/// \brief Set the velocity of the Velodyne
+		/// \param[in] _vel New target velocity
+		void SetVelocity(const double &_vel)
+		{
+		  // Set the joint's target velocity.
+		  this->model->GetJointController()->SetVelocityTarget(
+		      this->joint->GetScopedName(), _vel);
+		}
+
+	private:
+		/// \brief Handle incoming message
+		/// \param[in] _msg Repurpose a vector3 message. This function will
+		/// only use the x component.
+		void OnMsg(ConstVector3dPtr &_msg)
+		{
+		  this->SetVelocity(_msg->x());
+		}
+
 		/// \brief Pointer to the model.
-		private: physics::ModelPtr model;
+		physics::ModelPtr model;
 
 		/// \brief Pointer to the joint.
-		private: physics::JointPtr joint;
+		physics::JointPtr joint;
 
 		/// \brief A PID controller for the joint.
-		private: common::PID pid;
+		common::PID pid;
+
+		/// \brief A node used for transport.
+		transport::NodePtr node;
+
+		/// \brief A subscriber to a named topic.
+		transport::SubscriberPtr sub;
 	};
 	// Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
 	GZ_REGISTER_MODEL_PLUGIN(VelodynePlugin)
